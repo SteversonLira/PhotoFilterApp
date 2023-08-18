7661a38809346bc4d0cb9621909b3cd78a1fb486
SLcode <steversonlira@outlook.com.br>
2023-08-18 17:35:18 -0300

    Adicionar arquivos de projeto.

---
+++ PhotoFilterApp/frmPhotoFilterApp.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace PhotoFilterApp
+{
+    partial class frmPhotoFilterApp
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.photo = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.photo)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // photo
+            // 
+            this.photo.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.photo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.photo.Location = new System.Drawing.Point(0, 0);
+            this.photo.Name = "photo";
+            this.photo.Size = new System.Drawing.Size(1031, 606);
+            this.photo.TabIndex = 0;
+            this.photo.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(168, 606);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(8, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "FILTRO 01";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(8, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 52);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "FILTRO 02";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(8, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 52);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "FILTRO 03";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(8, 186);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 52);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "FILTRO 04";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(8, 244);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(153, 52);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "FILTRO 05";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.Location = new System.Drawing.Point(8, 302);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(153, 52);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "FILTRO 06";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.Location = new System.Drawing.Point(8, 360);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(153, 52);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "FILTRO 07";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button8.Location = new System.Drawing.Point(8, 418);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(153, 52);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "FILTRO 08";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button9.Location = new System.Drawing.Point(8, 476);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(153, 52);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "FILTRO 09";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button10.Location = new System.Drawing.Point(8, 534);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(153, 52);
+            this.button10.TabIndex = 0;
+            this.button10.Text = "FILTRO 10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // frmPhotoFilterApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 606);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.photo);
+            this.Name = "frmPhotoFilterApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmPhotoFilterApp_FormClosing);
+            this.Load += new System.EventHandler(this.frmPhotoFilterApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.photo)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox photo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PhotoFilterApp/frmPhotoFilterApp.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using DocumentFormat.OpenXml.Drawing;
+
+namespace PhotoFilterApp
+{
+    public partial class frmPhotoFilterApp : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        int filtroPadrao = 0;
+
+        public frmPhotoFilterApp()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPhotoFilterApp_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count == 0)
+            {
+                MessageBox.Show("Nenhuma webcam encontrada.");
+                return;
+            }
+
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+            videoSource.Start();
+        }
+
+        private void frmPhotoFilterApp_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+        }
+        private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap originalImage = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap filteredImage = ApplyFilter(originalImage, filtroPadrao); // Apply your desired filter function here
+
+            photo.Image = filteredImage;
+        }
+
+        private Bitmap ApplyFilter(Bitmap inputImage, int filterIndex)
+        {
+            // Apply your color filter here
+            // Example: Apply one of 10 different filters randomly
+
+            Random random = new Random();
+            //int filterIndex = random.Next(10);
+
+            IFilter filter;
+
+            switch (filterIndex)
+            {
+                case 0:
+                    filter = new AForge.Imaging.Filters.Grayscale(0.2125, 0.7154, 0.0721);
+                    break;
+                case 1:
+                    filter = new Invert();
+                    break;
+                case 2:
+                    filter = new Sepia();
+                    break;
+                case 3:
+                    filter = new ContrastCorrection(30);
+                    break;
+                case 4:
+                    filter = new SaturationCorrection((float)0.5);
+                    break;
+                case 5:
+                    filter = new BrightnessCorrection(20);
+                    break;
+                case 6:
+                    filter = new GaussianBlur(2, 11);
+                    break;
+                case 7:
+                    filter = new OilPainting();
+                    break;
+                case 8:
+                    filter = new Mirror(false, true);
+                    break;
+                case 9:
+                    filter = new RotateChannels();
+                    break;
+                default:
+                    return inputImage; // Return original image if no filter is applied
+            }
+
+            return filter.Apply(inputImage);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            filtroPadrao = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            filtroPadrao = 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            filtroPadrao = 2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            filtroPadrao = 3;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            filtroPadrao = 4;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            filtroPadrao = 5;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            filtroPadrao = 6;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            filtroPadrao = 7;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            filtroPadrao = 8;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            filtroPadrao = 9;
+        }
+    }
+}
